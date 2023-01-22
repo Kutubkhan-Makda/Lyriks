b070d17fadaa7cc4936770063b9a2ea6933a0542
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-22 21:58:58 +0530

    day-1

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -7,7 +7,9 @@ const Discover = () => {
         <div className="flex flex-col">
             <div className="flex w-full justify-between items-center sm:flex-row flex-col mt-4 mb-10">
                 <h2 className="font-bold text-3xl text-white text-left">Discover {generaTitle}</h2>
-                <select onChange={()=>{}}></select>
+                <select onChange={()=>{}} value='' className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0">
+                    {genres.map((gener)=><option>{gener.title}</option>)}
+                </select>
             </div>
         </div>
     );
