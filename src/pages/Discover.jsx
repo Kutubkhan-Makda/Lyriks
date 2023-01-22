9068fb4361b1a2dc4b55482cf29510b2b8aa7d51
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-22 13:38:57 +0530

    day-1

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,16 @@
-const Discover = () => <div>Discover</div>;
+import { genres } from "../assets/constants";
+
+const Discover = () => {
+    const generaTitle = 'pop';
+
+    return(
+        <div className="flex flex-col">
+            <div className="flex w-full justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+                <h2 className="font-bold text-3xl text-white text-left">Discover {generaTitle}</h2>
+                <select onChange={()=>{}}></select>
+            </div>
+        </div>
+    );
+};
 
 export default Discover;
