b70f0ada0ea60bb6e5e77fc1c2d0d366c24e512c
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-23 21:48:13 +0530

    day-2

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -13,8 +13,8 @@ const Discover = () => {
                 </select>
             </div>
             <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-                {[1,2,3,4,5,6,7,8,9,10].map((song)=>(
-                    <SongCard />
+                {[1,2,3,4,5,6,7,8,9,10].map((song,i)=>(
+                    <SongCard key={song.key} song={song} i={i}/>
                 ))}
             </div>
         </div>
