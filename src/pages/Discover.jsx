eaf1b7529c963ab5d070ef80d7df526fa75fdb04
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-01-23 21:42:18 +0530

    day-2

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,4 +1,5 @@
 import { genres } from "../assets/constants";
+import SongCard from "../components/SongCard";
 
 const Discover = () => {
     const generaTitle = 'pop';
@@ -8,8 +9,13 @@ const Discover = () => {
             <div className="flex w-full justify-between items-center sm:flex-row flex-col mt-4 mb-10">
                 <h2 className="font-bold text-3xl text-white text-left">Discover {generaTitle}</h2>
                 <select onChange={()=>{}} value='' className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0">
-                    {genres.map((gener)=><option>{gener.title}</option>)}
+                    {genres.map((gener)=><option key={gener.value} value={gener.value}>{gener.title}</option>)}
                 </select>
+            </div>
+            <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+                {[1,2,3,4,5,6,7,8,9,10].map((song)=>(
+                    <SongCard />
+                ))}
             </div>
         </div>
     );
